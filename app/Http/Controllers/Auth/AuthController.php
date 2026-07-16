5f9b34d9da03d718f01efe7d7c3d358d21ce84b8
Abu-Ahmad-0 <1abu2ahmad@gmail.com>
2026-07-16 10:12:21 +0300

    Set up authentication routes and controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+
+class AuthController
+{
+    public function register(Request $request) {}
+    public function login(Request $request) {}
+    public function logout(Request $request) {}
+    public function me(Request $request) {}
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
+
+require __DIR__ . '/api/auth.php';
