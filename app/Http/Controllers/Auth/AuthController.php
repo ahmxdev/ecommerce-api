73e920eba4edc8f6d8ee59a6e6585d1f77baa67e
Abu-Ahmad-0 <1abu2ahmad@gmail.com>
2026-07-16 12:09:44 +0300

    Implement user registration with Sanctum

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController
 {
-    public function register(Request $request) {}
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user = User::create($validated);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'user registered successfully',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
     public function login(Request $request) {}
     public function logout(Request $request) {}
     public function me(Request $request) {}
