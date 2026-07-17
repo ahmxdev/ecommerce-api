6bfdd46423e014ef2fc1c99a73a8d3cb5eea33da
Abu-Ahmad-0 <1abu2ahmad@gmail.com>
2026-07-17 22:48:28 +0300

    Add Address model and migration

---
+++ app/Models/Address.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable([
+    'user_id',
+    'country',
+    'state',
+    'city',
+    'district',
+    'street',
+    'building',
+    'floor',
+    'apartment',
+    'landmark',
+])]
+
+class Address extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
