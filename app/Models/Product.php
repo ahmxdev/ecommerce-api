cdce05a0deb4fff737f7c17393a9cb4e3c5fe0a4
Abu-Ahmad-0 <1abu2ahmad@gmail.com>
2026-07-17 17:42:09 +0300

    Add ProductImage model and migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,4 +17,8 @@ class Product extends Model
     {
         return $this->belongsTo(Brand::class);
     }
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
